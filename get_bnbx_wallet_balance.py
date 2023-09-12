20d7b59970eba5eef480f3f8f5805cd0c02cd24e
rio675 <emeth00@gmail.com>
2023-09-12 20:44:58 +0900

    added get_bnbx_wallet_balance.py

---
+++ get_bnbx_wallet_balance.py
@@ -0,0 +1,98 @@
+import requests
+from requests import Session
+
+def get_token_balance(bsc_api_key, contract_address, wallet_address):
+    api_url = "https://api.bscscan.com/api"
+
+    params = {
+        "module": "account",
+        "action": "tokenbalance",
+        "contractaddress": contract_address,
+        "address": wallet_address,
+        "tag": "latest",
+        "api_key": bsc_api_key
+    }
+
+    response = requests.get(api_url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        if "result" in data:
+            balance = data["result"]
+            return balance
+        else:
+            return None
+    else:
+        return None
+
+def convert_to_jpy(coinmarketcap_api_key, amount, symbol):
+    api_url = "https://pro-api.coinmarketcap.com/v2/tools/price-conversion"
+
+    params = {
+        "symbol": symbol,
+        "amount": amount,
+        "convert": "JPY"
+    }
+
+    headers = {
+        "Accepts": "application/json",
+        "X-CMC_PRO_API_KEY": coinmarketcap_api_key
+    }
+
+    session = Session()
+    session.headers.update(headers)
+    response = requests.get(api_url, params=params, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        try:
+            # 'price' の整数部分を取得
+            integer_price = int(data["data"][0]["quote"]["JPY"]["price"])
+            return integer_price
+        except (KeyError, ValueError):
+            return None
+    else:
+        print("API Request Failed. Status Code:", response.status_code)
+        print("API Response:")
+        try:
+            print(response.json())  # エラーレスポンスを表示
+        except ValueError:
+            print(response.text)  # エラーレスポンスがJSONでない場合、テキストとして表示
+        return None
+
+def smallest_decimal_to_normal(balance, decimal_places):
+    # Convert the smallest decimal representation to a normal decimal representation
+    return balance / (10 ** decimal_places)
+
+def truncate_to_range(number, min_value=1e-8, max_value=1000000000000):
+    # Coin market cap API指定範囲内に収まるように数値を制限
+    if number < min_value:
+        number = min_value
+    elif number > max_value:
+        number = max_value
+
+    # 数値を小数点以下8桁まで切り捨て
+    number = round(number, 8)
+
+    return str(number)
+
+if __name__ == "__main__":
+    bsc_api_key = "P8CHB9K1WSKYQXJ7BBDBPWZGER9U33DNDN"
+    coinmarketcap_api_key = "baefec12-bbb8-4e7e-845e-24bd574d0cdc"
+    contract_address = "0x1bdd3Cf7F79cfB8EdbB955f20ad99211551BA275"
+    wallet_address = "0x60270bF99480FfDC71f2453A38117DA2ED7a5c77"
+    decimal_places = 18
+
+    bnbx_balance_smallest_decimal = get_token_balance(bsc_api_key, contract_address, wallet_address)
+    bnbx_balance_normal_decimal = smallest_decimal_to_normal(int(bnbx_balance_smallest_decimal), decimal_places)
+    bnbx_balance_normal_decimal_truncated = truncate_to_range(bnbx_balance_normal_decimal, min_value=1e-8, max_value=1000000000000)
+    
+    if bnbx_balance_normal_decimal_truncated is not None:
+        print(f"BNBX残高: {bnbx_balance_normal_decimal_truncated}")
+        jpy_amount = convert_to_jpy(coinmarketcap_api_key, bnbx_balance_normal_decimal_truncated, "BNBX")
+        if jpy_amount is not None:
+            print(f"JPY残高: {jpy_amount} JPY")
+        else:
+            print("JPYへの変換ができませんでした。")
+    else:
+        print("BNBX残高が取得できませんでした。")
