503edcfc2d755d94362e50d3fa5b1e6b874d62f6
rio675 <emeth00@gmail.com>
2024-04-09 14:27:00 +0900

    updated to playwright from selenium

--- moneyforward_balance_updater.py
+++ moneyforward_balance_updater.py
@@ -1,8 +1,5 @@
 """module providing a lambda function to integrate crypto assets to MoneyForward."""
-import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from playwright.sync_api import sync_playwright
 
 from get_bnb_wallet_balance import get_bnb_balance
 from get_bnbx_wallet_balance import get_bnbx_balance
@@ -14,102 +11,86 @@ def lambda_handler(event, context):
     """
     メインのLambdaハンドラ関数
     """
-    try:
-        url = 'MONEYFORWARD_BANK_URL'
-        user = 'MY_EMAIL_ADDRESS'
-        password = 'MONEYFORWARD_PASSWORD'
-
-        driver = setup_browser()
-        login_to_moneyforward(driver, url, user, password)
-        update_moneyforward_balance(driver)
-    finally:
-        driver.quit()
-        print("End")
-
-def setup_browser():
-    """
-    ブラウザのセットアップを行う関数
-    """
+    url = 'MONEYFORWARD_BANK_URL'
+    user = 'MY_EMAIL_ADDRESS'
+    password = 'MONEYFORWARD_PASSWORD'
     # Chromeをheadlessモードで起動
-    driver_path ='/opt/headless/chromedriver'
-    options = webdriver.ChromeOptions()
-    options.binary_location = "/opt/headless/headless-chromium"
-    # ヘッドレスモードでChromeを起動
-    options.add_argument("--headless")
-    # セキュリティ設定の一部を無効化
-    options.add_argument("--no-sandbox")
-    # シングルプロセスモードでChromeを起動
-    options.add_argument('--single-process')
-    # /dev/shmの使用を無効化
-    options.add_argument('--disable-dev-shm-usage')
-    # WebページによるHeadless検出回避の為のダミーデータ
-    options.add_argument("user-agent=Mozilla/5.0\
-                            (Windows NT 10.0; Win64; x64)\
-                            AppleWebKit/537.36 (KHTML, like Gecko)\
-                            Chrome/58.0.3029.110 Safari/537.36")
+    with sync_playwright() as playwright:
+        # Open a new browser context
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context(
+            #Change user agent 以下のようにuser_agentを偽造しないと、MoneyForwardのログイン画面が表示されないため。
+            user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+        )
+        page = context.new_page()
+        page.goto(url)        
+        login_to_moneyforward(page, user, password)
+        update_moneyforward_balance(page)
+        context.close()
+        browser.close()
 
-    # pylint: disable=E1123
-    driver = webdriver.Chrome(executable_path=driver_path, options=options)
-    driver.implicitly_wait(10)
-    return driver
-
-def login_to_moneyforward(driver, url, user, password):
+def login_to_moneyforward(page, user, password):
     """
     マネーフォワードにログインする関数
     """
+    page.fill('#mfid_user\\[email\\]', user)
+    # Click the submit button
+    page.click('#submitto')
+    page.wait_for_load_state('networkidle')
+    # Fill in the password field
+    page.fill('#mfid_user\\[password\\]', password) 
+    # Click the submit (sign in) button
+    page.click('#submitto')
+    page.wait_for_load_state('networkidle')
+    return page
 
-    driver.get(url)
-    WebDriverWait(driver, 10)
-
-    elem = driver.find_element(By.ID, "mfid_user[email]")
-    elem.clear()
-    elem.send_keys(user)
-
-    button = driver.find_element(By.ID, "submitto")
-    button.click()
-
-    driver.implicitly_wait(3)
-
-    elem = driver.find_element(By.ID, "mfid_user[password]")
-    elem.clear()
-    elem.send_keys(password)
-
-    button = driver.find_element(By.ID, "submitto")
-    button.click()
-
-    driver.implicitly_wait(3)
-
-    button = driver.find_element(By.LINK_TEXT, "残高修正")
-    button.click()
-
-    driver.implicitly_wait(3)
-
-def update_moneyforward_balance(driver):
-    """
-    マネーフォワードの残高を更新する関数
-    """
-    total_balance = calculate_sum(get_bnb_balance(), get_btc_balance(), get_steth_balance())
-
-    elem = driver.find_element(By.ID, "rollover_info_value")
-    elem.clear()
-    elem.send_keys(total_balance)
-
-    driver.implicitly_wait(3)
-
-    form_element = driver.find_element(By.ID, 'rollover_form')
-    input_elements = form_element.find_elements(By.TAG_NAME, 'input')
-
-    commit_button = None
-    for input_element in input_elements:
-        if input_element.get_attribute('name') == 'commit':
-            commit_button = input_element
+def delete_all_cash_deposit(page):
+    # Click all delete buttons(削除ボタンがなくなるまで削除ボタンをクリックし続ける)
+    while True:
+        # Handle dialog (popup)　表示されるダイアログを自動的に承認（OKボタンを押す）する。
+        page.once("dialog", lambda dialog: dialog.accept())
+        # Find all delete buttons within the specified table
+        delete_buttons = page.query_selector_all('.table.table-bordered.table-depo .btn-asset-action[data-method="delete"]')
+        # If no more delete buttons, break the loop
+        if not delete_buttons:
             break
+        # Click the first delete button
+        delete_buttons[0].click()
+        # Wait for navigation (optional, depending on the page)
+        page.wait_for_load_state('networkidle')
 
-    commit_button.click()
-    time.sleep(10)
+def create_asset_in_mf(page, asset_type, asset_name, market_value):
+    #マネーフォワードの残高を新規作成する関数
+    page.get_by_role("button", name="手入力で資産を追加").click()
+    page.get_by_role("combobox", name="資産の種類").select_option(str(asset_type))
+    page.get_by_label("資産の名称").fill(str(asset_name)[:20])
+    page.get_by_label("現在の価値").fill(str(market_value)[:12])
+    page.get_by_role("button", name="この内容で登録する").click()
+    page.wait_for_timeout(2000)
+    page.wait_for_load_state('networkidle')
+    return True
 
-def calculate_sum(*balances):
-    """
-    残高の合計を計算する関数
-    """
-    return sum(balances)
+def update_moneyforward_balance(page):
+    #前の入力項目を全削除
+    delete_all_cash_deposit(page)
+    #66とはまMF内の資産の種類の’暗号資産’のこと
+    create_asset_in_mf(page, 66, 'BTC', get_btc_balance())
+    create_asset_in_mf(page, 66, 'BNB', get_bnb_balance())
+    create_asset_in_mf(page, 66, 'STETH', get_steth_balance())
+    return page
+
+if __name__ == "__main__":
+    event = {
+        "key1": "value1",
+        "key2": "value2",
+        "key3": "value3"
+    }
+    context = {
+        "function_name": "my_lambda_function",
+        "runtime": "python3.9",
+        "memory_limit_in_mb": 128,
+        "timeout": 30,
+        "log_group_name": "/aws/lambda/my_lambda_function",
+        "request_id": "1234567890"
+    }
+    lambda_handler(event, context)
